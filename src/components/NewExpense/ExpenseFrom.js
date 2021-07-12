258d5bc675b0874776cfcd1fc6b68f9f81dc38b0
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-07-12 07:27:07 +0700

    Add submitHandler function

--- src/components/NewExpense/ExpenseFrom.js
+++ src/components/NewExpense/ExpenseFrom.js
@@ -31,12 +31,34 @@ const ExpenseForm = () => {
     });
   };
 
+  const submiteHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: userInput.enteredTitle,
+      amount: userInput.enteredAmount,
+      date: new Date(userInput.enteredDate),
+    };
+
+    console.log(expenseData);
+
+    setUserInput({
+      enteredTitle: '',
+      enteredAmount: 0,
+      enteredDate: new Date(),
+    });
+  };
+
   return (
-    <form>
+    <form onSubmit={submiteHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input
+            type='text'
+            value={userInput.enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -44,6 +66,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            value={userInput.enteredAmount}
             onChange={amoutChangeHandler}
           />
         </div>
@@ -53,6 +76,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             step='2022-12-31'
+            value={userInput.enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
