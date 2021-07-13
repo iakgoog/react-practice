d71a592b6ce68474c04620e702fdaf58b090e8fd
Sutthinart Khunvadhana <iakgoog@gmail.com>
2021-07-14 05:26:41 +0700

    Add toggle button to display/hide 'Add Expense' form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExense.css';
 
 const NewExpense = (props) => {
+  const [displayForm, setDisplayForm] = useState(false);
   const saveExpenseDateHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -12,11 +13,28 @@ const NewExpense = (props) => {
     props.onAddExpense(expenseData);
   };
 
-  return (
-    <div className='new-expense'>
-      <ExpenseForm onSaveExpenseDate={saveExpenseDateHandler} />
+  const hideForm = () => {
+    setDisplayForm(false);
+  };
+
+  const showForm = () => {
+    setDisplayForm(true);
+  };
+
+  const expenseForm = displayForm ? (
+    <ExpenseForm
+      onSaveExpenseDate={saveExpenseDateHandler}
+      hideForm={hideForm}
+    />
+  ) : (
+    <div className='button-wrapper'>
+      <button type='button' onClick={showForm}>
+        Add New Expense
+      </button>
     </div>
   );
+
+  return <div className='new-expense'>{expenseForm}</div>;
 };
 
 export default NewExpense;
